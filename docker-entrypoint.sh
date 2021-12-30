f0df848dcf7d91e520c39d0839f7f953dd49109f
Jobggun <jobggun@kakao.com>
2021-12-30 22:03:26 +0900

    docker entrypoint update with healthcheck

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,26 @@
 
 function process_signal()
 {
+    echo '[INFO] termination signal received'
+    ./run_hlstats stop
     kill -TERM $cronpid
     wait $cronpid
+    echo '[INFO] terminated'
+    exit
 }
 
-trap process_signal SIGTERM SIGINT SIGQUIT SIGHUP ERR
+if [ $1 = 'cron' ]
+then
+    echo '[INFO] HLstatsX daemon starting'
+    ./run_hlstats start
+    echo '[INFO] HLstatsX daemon started'
 
-if [ $1 = 'cron' ]; then
+    echo '[INFO] cron starting'
     cron -f &
-
     cronpid=$!
+    echo '[INFO] cron started'
 
+    trap process_signal SIGTERM SIGINT SIGQUIT SIGHUP ERR
     wait $cronpid
 else
     exec $@
